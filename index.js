a5010d8d7d0794471f593f18b2e82bdb967a7640
silvianaim02 <silvianaim68@gmail.com>
2022-06-06 16:23:26 +0700

    change site name

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://clientch7.netlify.app",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
